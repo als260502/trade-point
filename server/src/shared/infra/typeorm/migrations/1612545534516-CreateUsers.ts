57a94fa6f20e05fc508e3d51dbb5f07b58e76b19
Andre <als260602@msn.com>
2021-02-05 15:46:53 -0300

    configurando ambiente pra TDD

---
+++ server/src/shared/infra/typeorm/migrations/1612545534516-CreateUsers.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateUsers1612545534516 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> { }
+
+  public async down(queryRunner: QueryRunner): Promise<void> { }
+}
